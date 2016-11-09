d556373edc22fdf1fd13375b69e3924338b1d5f1
Shivam Kumar <shivamhclme5@gmail.com>
2016-11-09 15:56:44 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,29 @@
+  
+public class Employee {
+	
+	   String name;
+	   int id;
+	   double basicSalary;
+	   
+	   
+	   
+	   
+	public Employee(String name, int id, double basicSalary) {
+		this.name = name;
+		this.id = id;
+		this.basicSalary = basicSalary;
+		
+	}
+	
+	public double calculateSalary(){
+		
+		double salary = basicSalary;
+		return salary;
+		
+	}
+	   
+	   
+	   
+	   
+
+}
